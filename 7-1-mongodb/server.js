bb61d4e4edddeccc548925d9f1cf6c9a3278d869
abdullah-fz <abdullahalzahim1@gmail.com>
2026-04-13 11:01:36 +0300

    Mongoose TODO 3 create documents

--- 7-1-mongodb/server.js
+++ 7-1-mongodb/server.js
@@ -1,19 +1,29 @@
 import mongoose from "mongoose";
 
 const MONGO_URI =
-  "mongodb+srv://abdullahfz:YOUR_PASSWORD@cluster0.zloieip.mongodb.net/labDB?retryWrites=true&w=majority&appName=Cluster0";
+  "mongodb+srv://abdullahfz:abdullahfz@cluster0.zloieip.mongodb.net/labDB?retryWrites=true&w=majority&appName=Cluster0";
 
 const studentSchema = new mongoose.Schema({
   name: String,
   age: Number,
   major: String,
 });
-//aa
+
 const Student = mongoose.model("Student", studentSchema);
+
+async function createStudents() {
+  await Student.insertMany([
+    { name: "Ali", age: 21, major: "CS" },
+    { name: "Sara", age: 23, major: "SE" },
+  ]);
+  console.log("✅ Inserted");
+}
 
 try {
   await mongoose.connect(MONGO_URI);
   console.log("✅ Connected to MongoDB");
+
+  await createStudents();
 } catch (error) {
   console.error("❌ Error:", error.message);
 } finally {
