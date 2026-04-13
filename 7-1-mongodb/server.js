4267809f039efefc297df942729844979718063d
abdullah-fz <abdullahalzahim1@gmail.com>
2026-04-13 10:57:06 +0300

    Mongoose TODO 2 schema and model

--- 7-1-mongodb/server.js
+++ 7-1-mongodb/server.js
@@ -3,6 +3,14 @@ import mongoose from "mongoose";
 const MONGO_URI =
   "mongodb+srv://abdullahfz:YOUR_PASSWORD@cluster0.zloieip.mongodb.net/labDB?retryWrites=true&w=majority&appName=Cluster0";
 
+const studentSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+  major: String,
+});
+//aa
+const Student = mongoose.model("Student", studentSchema);
+
 try {
   await mongoose.connect(MONGO_URI);
   console.log("✅ Connected to MongoDB");
